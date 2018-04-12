5fa0d0759391b99d9c33bd7e3b2587f3d980c671
Zenohate <zenohate@gmail.com>
2018-04-12 22:26:18 +0200

    pre-test commit

--- src/foreign_key.js
+++ src/foreign_key.js
@@ -24,12 +24,19 @@ function insert_csv(path,options)
 		mongoose_foreign_model: {},
 		foreign_key: "ownerId",
 		primary_key: "_rowid",
-		on_err:(err)=>{console.log(err)}
+		on_err:(err)=>{console.log(err);}
 	};
 	var f_options = Object.assign({}, defaults, options);
 	if(f_options.use_fk)
 	{
-
+		reader.CSV_SyncParse(path,f_options.separator,(data)=>{
+			var predicate={};
+			predicate[f_options.link_key_name]=data[f_options.link_local_key_name];
+			var foreign_doc=f_options.mongoose_foreign_model.find(predicate);
+			data[f_options.foreign_key]=foreign_doc[f_options.primary_key];
+			var doc = new f_options.mongoose_model(data);
+			doc.save(f_options.on_err);
+		});
 	}
 	else
 	{
